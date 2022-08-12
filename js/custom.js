d1f6647727ca5b57c55b60ec532f71345d8e083c
Gmfmf12 <gmfgmf12@hotmail.com>
2022-08-12 11:27:17 -0300

    Sendmail e modal ok je4npw

--- js/custom.js
+++ js/custom.js
@@ -26,14 +26,32 @@ $(function () {
   });
 });
 
+// mascara telefone input
+var behavior = function (val) {
+  return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+},
+  options = {
+    onKeyPress: function (val, e, field, options) {
+      field.mask(behavior.apply({}, arguments), options);
+    }
+  };
+$('.phone').mask(behavior, options);
+
+//função sendmail ajax
 function sendmail() {
   dataEmail = $('form.modalform').serializeArray()
   $.ajax({
     type: "post",
-    url: "https://apidesastre.000webhostapp.com/sendmessage.php",
+    url: "https://apidesastre.000webhostapp.com/32498759023841382/sendmessage.php",
     data: dataEmail,
     success: function (response) {
-      alert(response)      
+      alert(response)
+      $('form :input').val('');
+      $('#membershipForm').modal('hide')
     }
   });
 };
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+
